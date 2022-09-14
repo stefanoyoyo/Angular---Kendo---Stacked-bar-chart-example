46d303e9abab05fc1cda6b7ed0317e9f784a982f
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-09-14 15:12:15 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
       <kendo-chart>
         <kendo-chart-series>
           <!--blue                            (Y1, Y2, Y3)-->
-          <kendo-chart-series-item type="bar" [data]="[1, 2, 3]" >
+          <kendo-chart-series-item type="bar" [data]="[1, 2, 3]" [stack]="{ group: 'a' }">
           </kendo-chart-series-item>
           <!--Yellow                          (Y1,Y2, Y3)-->
-          <kendo-chart-series-item type="bar" [data]="[1, 1, 3]" >
+          <kendo-chart-series-item type="bar" [data]="[1, 1, 3]" [stack]="{ group: 'a' }">
           </kendo-chart-series-item>
           <!--Green                           (Y1,Y2, Y3)-->
-          <kendo-chart-series-item type="bar" [data]="[1, 1, 2]" >
+          <kendo-chart-series-item type="bar" [data]="[1, 1, 2]" [stack]="{ group: 'a' }">
           </kendo-chart-series-item>
         </kendo-chart-series> 
       </kendo-chart>
